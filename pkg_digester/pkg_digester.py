c13ae3128f13700ec66f89df8b00fef4c5ec4b75
John-Patrik Nilsson <themindbug@gmail.com>
2010-06-15 10:32:14 +0200

    Scribbled some code on the pkg_digester, looks horribad.

--- pkg_digester/pkg_digester.py
+++ pkg_digester/pkg_digester.py
@@ -1,42 +1,42 @@
-# Maintainer: <mindbug> John-Patrik Nilsson, themindbug@gmail.com
-# Copyright info
+#! /usr/bin/env python
+# Maintainer: John-Patrik Nilsson, IRC: mindbug, themindbug@gmail.com
+# TODO
+# Copyright 
 
+""" pkg_digester: a module for translating a pickle stream of pkgcore_checks objects into a human-readable collection of strings.
 """
-	Pseudo code
-	
-	get pickled stream from pkgcore_check (or wherever), the stream is probably a Reporter-object containing Result-objects
-
-	unpickle the stream
-
-	is there a way to convert objects into SQL? ORM, SQLAlchemy?
-
-	loop through the stream and input the objects into a SQLite3 db
-
-	TODO
-		read about for
-		and read about try (if the classes miss one attribute)
-		get common "fields" for the check-objects
-		hint: pkgcore_checks.base.Result
-"""
-#from pkgcore.config import load_config
-#repo_string = '/usr/portage'
-#repo = load_config().repo[repo_string]
-
-#file = open('repo.pkl', 'w')
-#p = snakeoil.pickling.Pickler(repo, file)
-#file.close()
-
-# we want to use this to grab the pickle stream...
 from snakeoil.pickling import iter_stream
-# open the ready-made binary pickle file, containing 
-# pkgcore_checks.base.Result objects, or derivate 
-# objects of that class.
-pickle_file = open("qa-results.pcheck")
-# iter_stream = class generator
-# create a list of the contents of the pickled file
-# with the help of the generator object iter_stream
-check_list = list(iter_stream(pickle_file))
-# check_list[0] is a list header!
-print check_list[1]
-dir(check_list[1])
-# repr(checks[6]).split()[0]
+
+def load_pickles(file):
+    """load_pickles(file='qa-results.pcheck'): returns a tuple of pcheck result objects from the given pickle file."""
+    return tuple(iter_stream(open(file)))
+
+# TODO
+#   Get it to work..
+# Notes:
+#   The pickled objects can be represented as tuples of
+#   len(_keywords) elements.
+def formatter(tuple):
+    """formatter(tuple): formats and organizes the objects in the tuple, returns a tuple."""
+    # Formatting keywords; the stuff I want to save.
+    _keywords = (".package", ".version", "category",
+                ".short_desc", ".keywords", ".long_desc")
+    # Create and return a list containing objects converted into tuples.
+    _formatted_list = []
+    return _formatted_list
+
+def text_outputter(tuple):
+    for item in tuple[1:]:
+        classname = str(item.__class_).strip("<class '>").split(".")[-1].lower()
+        print (item.category, item.package, item.version, 
+        classname, item.short_desc)
+
+def main():
+    checks = load_pickles(qa-results.pcheck)
+    checks = formatter(checks)
+
+# If called as a script call main(), otherwise be a module.
+if __name__ == '__main__':
+    main()
+else:
+    print("I'm a module! :O")
