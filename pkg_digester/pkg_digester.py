ca31a038b4b49690c4313a365abf55029d4f1350
John-Patrik Nilsson <themindbug@gmail.com>
2010-06-16 01:45:14 +0200

    The digester outputs nicely now, to stdout anyways. Next task is to get it to write to a database.

--- pkg_digester/pkg_digester.py
+++ pkg_digester/pkg_digester.py
@@ -1,68 +1,96 @@
 #! /usr/bin/env python
-# Maintainer: John-Patrik Nilsson, IRC: mindbug, themindbug@gmail.com
-# TODO Copyright 
+# Maintainer: John-Patrik Nilsson 
+#             IRC: <mindbug> @ #gentoo-soc, #gentoo-dev, #pkgcore
+#             themindbug@gmail.com
 
-# This script does not define any objects, nor does it use
-# an Object Oriented approach to the database interaction.
-# Suggestions: Define each of the pickled objects as a new
-# kind of object, just for this script. Could make it cleaner.
-# Also, perhaps using SQLAlchemy to interface with the db?
-# But, for now, I consider that premature optimization.
-
-""" pkg_digester: a module for translating a pickle stream of pkgcore_checks objects into a human-readable collection of strings.
+""" pkg_digester: a module for translating a pickle stream of 
+    pkgcore_checks objects into a human-readable collection of strings.
 """
+
 from snakeoil.pickling import iter_stream
 
-def load_pickles(file):
-    """ load_pickles(file='qa-results.pcheck'): returns a tuple of pcheck result objects from the given pickle file.
+""" This script does not define any objects, nor does it use an Object 
+    Oriented approach to the database interaction.  
+    Suggestions: Define each of the pickled objects as a new kind of 
+    object, just for this script. Could make it cleaner.  Also, perhaps 
+    using SQLAlchemy to interface with the db?  But, for now, I consider 
+    that premature optimization.
+"""
+
+# TODO
+# Copyright crap?
+#
+# Make all attribute names Pythonic.
+#
+# Change and copy the result items so that the rows which are put out does
+# not have more than one version. This means that not two rows have an 
+# identical combination of category, package, and version.
+
+def digest_to_stdout(picklefile):
+    """ digest_to_stdout(filename): Formats and organizes the pcheck result 
+        objects from the given file into tuples, which then are output to
+        stdout.
+    """
+    unformatted_list = _load_pickles(picklefile)
+    # formatted_list = []
+    for item in unformatted_list[1:-1]:
+        # formatted_item = _format_result(item)
+        # formatted_list.append(formatted_item)
+        # print formatted_item
+        print _format_result(item)
+    # return formatted_list
+
+
+def digest_to_db(picklefile):
+    """ digest_to_db(picklefile): Formats and organizes the pcheck result 
+        objects from the given file into a list of tuples, and consumes 
+        them into a database.
+    """
+    unformatted_list = _load_pickles(picklefile)
+
+    pass
+
+
+def _load_pickles(file):
+    """ _load_pickles(file): Unpickles the given file and returns a tuple 
+        containing the objects within.
     """
     return tuple(iter_stream(open(file)))
 
-# TODO
-# Notes:
-#   The pickled objects can be represented as tuples.
-#   We want there to be only one row in the list with
-#   the combined values of category, package, and version.
-#   So, for all versions of the package:
-#       create a new row with identical other_stuff.
-def formatter(tuple):
-    """ formatter(tuple): formats and organizes the objects in a list of tuples, returns a list.
-    """
-    formatted_list = []
-    for item in tuple[1:]:
-        version = " ".join(item.version)
-        formatted_list.append((item.category, item.package, item.version, classname, item.short_desc))
-    return _formatted_list
 
-def gen_tuple(result_obj):
-    """ gen_tuple(result_obj): outputs a tuple with pcheck info.
+def _format_result(result_obj):
+    """ format_result(result_obj): returns a dictionary with 
+        pcheck info as strings.
     """
-    # Category
-    category = result_obj.category
-    # Package
-    package = result_obj.package
-    # Version
-    # TODO I want the versions not as a concatenated str, but as a
-    # new object.
     try:
-        version = " ".join(result_obj.version)
-    # Class
-    classname = str(result_obj.__class_).strip("<class '>").split(".")[-1].lower()
-    # Short description
+        category = result_obj.category
+    except AttributeError:
+        category = "n/a"
+    try:
+        package = result_obj.package
+    except AttributeError:
+        package = "n/a"
+    try:
+        version = result_obj.version
+        if isinstance(version, tuple):
+            version = " ".join(version)
+    except AttributeError:
+            version = "n/a"
+    classname = (str(result_obj.__class__).strip("<class '>").
+                 split(".")[-1].lower())
     short_desc = result_obj.short_desc
-    # Keywords
     try:
         keywords = " ".join(result_obj.keywords).strip()
-    except AttributeError as error:
-        keywords = "None"
-    return (category, package, version, keywords, classname)
+    except AttributeError:
+        keywords = "n/a"
+    return {'category': category, 'package': package, 'version': version, 
+            'keywords': keywords, 'class': classname}
+
 
 def main():
-    checks = load_pickles("qa-results.pcheck")
-    checks = formatter(checks)
+    pickle_file = "qa-results.pcheck"
+    digest_to_stdout(pickle_file)
 
-# If called as a script call main(), otherwise be a module.
+# If called as a script call main(), otherwise be a module (be passive).
 if __name__ == '__main__':
     main()
-else:
-    print("I'm a module! :O")
