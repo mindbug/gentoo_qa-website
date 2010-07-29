0697b454624ae74df5c2f1e1c7edf22b891a8072
mindbug <themindbug@gmail.com>
2010-07-29 14:07:15 +0200

    Decoupled the CSS from the templates.

--- django_gentoo_pkg/simple_qa/urls.py
+++ django_gentoo_pkg/simple_qa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic import list_detail
 from simple_qa.models import QAReport
 from simple_qa.views import *
+from django.conf import settings
 
 
 qareport_dict = {'queryset': QAReport.objects.all(),
@@ -14,4 +15,6 @@ urlpatterns = patterns('',
     #(r'^qareports/page(?<page>[0-9]+)/$', qareports),
     (r'^qareports/$', qareports),
     (r'^search/$', search),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 )
